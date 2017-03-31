29e7e670e1cc3407005bb66176fa531253754310
Eric D. Reichert <eric@reichertconsulting.com>
2017-03-30 21:43:48 -0400

    Removed unused test data.

--- src/record_handling.rs
+++ src/record_handling.rs
@@ -64,18 +64,13 @@ fn aggregate_record(aggregate_record: AggregateELBRecord,
 }
 
 #[cfg(test)]
-mod record_handling_tests {
+mod aggregate_records_tests {
 
     use std::collections::HashMap;
     use super::AggregateELBRecord;
     use super::aggregate_record;
     use chrono::{DateTime, UTC};
     use std::net::SocketAddrV4;
-
-    const TEST_RECORD: &'static str = "2015-08-15T23:43:05.302180Z elb-name 172.16.1.6:54814 \
-    172.16.1.5:9000 0.000039 0.145507 0.00003 200 200 0 7582 \
-    \"GET http://some.domain.com:80/path0/path1?param0=p0&param1=p1 HTTP/1.1\"\
-    ";
 
     #[test]
     fn inserting_two_records_with_different_values_creates_two_entries_each_recorded_once() {
