404b8c983e8960b2c81e6c5c98a017380fe28856
Eric D. Reichert <eric@reichertconsulting.com>
2017-04-23 20:53:27 -0400

    Created a Runner and added tests for it.

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use counter::file_handling;
 use counter::aggregation_control::AggregationController;
 use std::io::Write;
 use std::sync::mpsc;
-
+use std::path::PathBuf;
 
 const EXIT_SUCCESS: i32 = 0;
 const EXIT_FAILURE: i32 = 1;
@@ -39,22 +39,9 @@ fn main() {
         Ok(ref mut filenames) => {
             let num_files = filenames.len();
             debug!("Found {} files.", num_files);
-            let mut file_handling_msg_senders = Vec::new();
-            let (agg_msg_sender, agg_msg_receiver) = mpsc::channel::<_>();
-            let pool = sp::Pool::new(num_cpus::get());
-            for sender_id in 0..pool.workers() {
-                let (file_handling_msg_sender, file_handling_msg_receiver) = mpsc::channel::<_>();
-                file_handling_msg_senders.push(file_handling_msg_sender);
-                let cloned_agg_msg_sender = agg_msg_sender.clone();
-                pool.spawn(move || {
-                               file_handling::FileAggregator::new(sender_id)
-                                   .run(&file_handling_msg_receiver, &cloned_agg_msg_sender);
-                           });
-            }
 
-            let mut agg_control = AggregationController::new(agg_msg_receiver,
-                                                             file_handling_msg_senders);
-            let final_agg = agg_control.run_aggregation(filenames);
+            let mut runner = Runner::new();
+            let final_agg = runner.run(num_cpus::get(), filenames);
 
             debug!("Processed {} records in {} files.",
                    final_agg.num_raw_records,
@@ -78,7 +65,7 @@ fn main() {
                          time.num_milliseconds(),
                          final_agg.aggregation.len());
             }
-            pool.shutdown();
+            runner.shutdown();
             EXIT_SUCCESS
         }
 
@@ -91,6 +78,56 @@ fn main() {
     };
 
     std::process::exit(exit_code);
+}
+
+struct Runner {
+    thread_pool: sp::Pool,
+    file_handling_msg_senders: Vec<mpsc::Sender<file_handling::FileHandlingMessages>>,
+}
+
+impl Runner {
+
+    fn new() -> Runner {
+        Runner {
+            thread_pool: sp::Pool::empty(),
+            file_handling_msg_senders: Vec::new(),
+        }
+    }
+
+    fn run(&mut self, num_file_aggregators: usize, mut filenames: &mut Vec<PathBuf>) -> counter::FileAggregation {
+        let (agg_msg_sender, agg_msg_receiver) = mpsc::channel::<_>();
+        for sender_id in 0..num_file_aggregators {
+            let (file_handling_msg_sender, file_handling_msg_receiver) = mpsc::channel::<_>();
+            self.file_handling_msg_senders.push(file_handling_msg_sender);
+            let cloned_agg_msg_sender = agg_msg_sender.clone();
+            self.thread_pool.expand();
+            self.thread_pool.spawn(move || {
+                file_handling::FileAggregator::new(sender_id)
+                    .run(&file_handling_msg_receiver, &cloned_agg_msg_sender);
+            });
+        }
+        let mut agg_control = AggregationController::new(agg_msg_receiver,
+                                                         self.file_handling_msg_senders.clone());
+        agg_control.run_aggregation(filenames)
+    }
+
+    #[cfg(test)]
+    fn num_threads_in_pool(&self) -> usize {
+        self.thread_pool.workers()
+    }
+
+    #[cfg(test)]
+    fn num_file_handling_msg_senders(&self) -> usize {
+        self.thread_pool.workers()
+    }
+
+    fn shutdown(&mut self) -> () {
+        // Shutting down the file handlers is a precaution to prevent hangs and aids in testing.
+        for msg_sender in self.file_handling_msg_senders.iter() {
+            let _ = msg_sender.send(counter::file_handling::FileHandlingMessages::Done);
+        }
+        self.thread_pool.shutdown()
+    }
 }
 
 const LOG_LOCATION_ARG: &'static str = "log-location";
@@ -134,6 +171,40 @@ impl<'a> RuntimeContext<'a> {
 
     fn log_location(&self) -> &Path {
         Path::new(self.arg_matches.value_of(LOG_LOCATION_ARG).unwrap())
+    }
+}
+
+#[cfg(test)]
+mod runner_tests {
+
+    use std::path::PathBuf;
+
+    #[test]
+    fn runner_should_create_the_same_number_of_file_handling_message_senders_as_host_cpus() {
+        let num_cpus = ::num_cpus::get();
+        let mut files = Vec::new();
+        files.push(PathBuf::from("./test_artifacts/test_elb_log_file.log"));
+        let mut runner = super::Runner::new();
+
+        let _ = runner.run(num_cpus, &mut files);
+
+        assert_eq!(runner.num_file_handling_msg_senders(), num_cpus);
+
+        runner.shutdown()
+    }
+
+    #[test]
+    fn runner_should_create_a_thread_pool_having_the_same_number_of_cpus_as_the_host() {
+        let num_cpus = ::num_cpus::get();
+        let mut files = Vec::new();
+        files.push(PathBuf::from("./test_artifacts/test_elb_log_file.log"));
+        let mut runner = super::Runner::new();
+
+        let _ = runner.run(num_cpus, &mut files);
+
+        assert_eq!(runner.num_threads_in_pool(), num_cpus);
+
+        runner.shutdown()
     }
 }
 
